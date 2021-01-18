554acf53834b1b6a41a6554d22a8caee7e15ac48
Ryota <rytswd@gmail.com>
2021-01-18 00:46:55 +0000

    Add istio-operator dump setup

---
+++ tools/internal/update-istio-operator-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+__tools_dir=$(dirname "$0")/..
+__root_dir="$__tools_dir"/..
+
+for e in "armadillo" "bison" "dolphin"; do
+    istioctl operator dump >"$__root_dir"/clusters/$e/istio-setup/istio-operator-install.yaml
+done
