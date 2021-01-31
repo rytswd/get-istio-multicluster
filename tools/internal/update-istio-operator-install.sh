f16fb2bbf43e1b227257d3d22d948af76032444a
Ryota <rytswd@gmail.com>
2021-01-31 15:04:58 +0000

    Update istio-operator path

--- tools/internal/update-istio-operator-install.sh
+++ tools/internal/update-istio-operator-install.sh
@@ -5,5 +5,5 @@ __root_dir="$__tools_dir"/..
 
 for e in "armadillo" "bison" "dolphin"; do
     istioctl operator dump >"$__root_dir"/clusters/$e/argocd/stack/istio/istio-operator-install.yaml
-    istioctl operator dump >"$__root_dir"/clusters/$e/istio-setup/istio-operator-install.yaml
+    istioctl operator dump >"$__root_dir"/clusters/$e/istio-operator/istio-operator-install.yaml
 done
