744c926b60db038e8ee8f51adab1472e4e66e4eb
Ryota <rytswd@gmail.com>
2021-02-05 15:38:56 +0000

    Update Prometheus directory structure

--- tools/internal/update-prometheus-operator-install.sh
+++ tools/internal/update-prometheus-operator-install.sh
@@ -5,5 +5,5 @@ __root_dir="$__tools_dir"/..
 
 for e in "armadillo" "bison" "dolphin"; do
     curl -sSL "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.45.0/bundle.yaml" \
-        >"$__root_dir"/clusters/$e/observability/prometheus-operator/prometheus-operator-install.yaml
+        >"$__root_dir"/clusters/$e/observability/prometheus/operator-install/prometheus-operator-install.yaml
 done
